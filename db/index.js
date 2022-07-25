c9b76268a9895a1913f27dd46cc841bf670be082
Steve Morello <smorello4@gmail.com>
2022-07-25 09:48:45 -0400

    Setup db/index.js

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+// require('dotenv').config()
+
+// let dbUrl =
+//   process.env.NODE_ENV === 'production'
+//     ? process.env.MONGODB_URI
+//     :
+
+let MONGODB_URI = 'mongodb://127.0.0.1:27017/dejaBrewDatabase'
+
+mongoose
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('Successfully connected to MongoDB.')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+
+const db = mongoose.connection
+
+module.exports = db
