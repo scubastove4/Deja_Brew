22b55658e407197d4999f9d8fda28c1bb365a9f2
Steve Morello <smorello4@gmail.com>
2022-07-25 10:00:52 -0400

    Setup models/Beer.js

---
+++ models/Beer.js
@@ -0,0 +1,10 @@
+const { Schema } = require('mongoose')
+
+const Beer = new Schema({
+  image: { type: String, required: true },
+  beer_name: { type: String, required: true },
+  brewery: { type: String, required: true },
+  beer_type: { type: String, required: true },
+  avg_rating: { type: String, required: false },
+  num_of_reviews: { type: String, required: false }
+})
