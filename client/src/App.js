22336bbe3bf7f0281a1ff29174ffe4d2f841ea65
Steve Morello <smorello4@gmail.com>
2022-07-25 22:34:41 -0400

    Install axios. Setup App.js Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
+import { Routes, Route } from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import About from './pages/About'
+import BeerTypes from './pages/BeerTypes'
+import BeerDetails from './pages/BeerDetails'
+import RandomBeer from './pages/RandomBeer'
+import ReviewDetails from './pages/ReviewDetails'
+
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<BeerTypes />} />
+          <Route path="/" element={<BeerDetails />} />
+          <Route path="/" element={<RandomBeer />} />
+          <Route path="/" element={<ReviewDetails />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
