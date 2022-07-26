885f490897de4c7069eacbc0a82e35b68b81b7e1
Steve Morello <smorello4@gmail.com>
2022-07-25 22:45:50 -0400

    Add Links to NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/">About</Link>
+      <Link to="/">Random Beer</Link>
+      <Link to="/">Beer Types</Link>
+    </nav>
+  )
 }
 
 export default NavBar
