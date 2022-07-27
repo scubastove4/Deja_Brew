5397060387ed2d882d80140d9077c88bd787d683
Steve Morello <smorello4@gmail.com>
2022-07-27 12:25:19 -0400

    Add .env and Google Maps API Key to .env

--- client/src/pages/Breweries.js
+++ client/src/pages/Breweries.js
@@ -6,26 +6,32 @@ const Breweries = () => {
     lat: '',
     long: ''
   })
+  const [error, setError] = useState('')
+
   useEffect(() => {
-    const getLocation = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getUserLocation)
-      } else {
-        console.log('Not available')
-      }
-    }
-    const getUserLocation = (position) => {
-      setUserLocation({
+    const getUserLocation = async (position) => {
+      await setUserLocation({
         ...userLocation,
         lat: position.coords.latitude,
         long: position.coords.longitude
       })
-      console.log(userLocation)
+      console.log(position)
+    }
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getUserLocation, (error) => {
+          console.error(`Error = ${error.code}: ${error.message}`)
+          setError(error.message)
+        })
+      } else {
+        console.log('Not available')
+      }
     }
     getLocation()
   }, [])
 
-  // geolocation help https://youtu.be/U3dLjHN0UvM
+  // geolocation help https://youtu.be/U3dLjHN0UvM and https://www.pluralsight.com/guides/how-to-use-geolocation-call-in-reactjs
+
   return <div>Breweries</div>
 }
 
