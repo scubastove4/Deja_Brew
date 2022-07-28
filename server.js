fc18275de8aa60ad758c189417aa1e2433c3a1a0
Steve Morello <smorello4@gmail.com>
2022-07-28 19:50:04 -0400

    remove commented out in server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-// require('dotenv').config()
 
 const routes = require('./routes')
 const db = require('./db')
