d82868068ca51e96a549487992fbda9ac5a04290
Steve Morello <smorello4@gmail.com>
2022-08-02 09:39:36 -0400

    add about me and urlencoded middleware

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,28 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <div>
+      <h2>About the Developer</h2>
+      <p>
+        Hi! I'm Steve Morello, an NYC/LI-based full-stack software engineer.
+        This app is my second project completed in the General Assembly Software
+        Engineering Immersive. The application was created in a week and is
+        built on a MERN-stack.
+        <br />
+        My inspiration for Déjà Brew is Untappd, an app I use basically every
+        weekend to log new beers I've tried. Déjà Brew is a bit more public,
+        mostly because we haven't covered user auth, yet, but that's a planned
+        update. Additional updates include incorporating Google Maps to show the
+        location of nearby breweries and making the app responsive, so it is
+        more user-friendly on different sized devices.
+        <br />
+        Hope you enjoy it!
+        <br />
+        <a href="https://github.com/scubastove4">GitHub</a> |{' '}
+        <a href="https://www.linkedin.com/in/stephen-morello/">LinkedIn</a> |{' '}
+        <a href="https://twitter.com/scubastove4">Twitter</a>
+      </p>
+    </div>
+  )
 }
 
 export default About
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', routes)
