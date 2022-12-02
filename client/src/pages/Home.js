a6d0822f5b1d4b695773506e26062742be1694f4
Steve Morello <smorello4@gmail.com>
2022-12-02 14:43:57 -0500

    removed quotes from mngdb uri env

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,8 +19,6 @@ const Home = () => {
     setSearchQuery(e.target.value)
   }
 
-  console.log(process.env.MONGODB_URI)
-
   const changeSearchType = (e) => {
     toggleSearchType(e.target.value)
     toggleSearchUrl(e.target.name)
